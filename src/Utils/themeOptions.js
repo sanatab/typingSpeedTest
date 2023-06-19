9e86eb49257405dd6ddc8bf975e9303dabbe3151
sanatab <120128180+sanatab@users.noreply.github.com>
2023-06-19 18:44:15 +0530

    done

---
+++ src/Utils/themeOptions.js
@@ -0,0 +1,92 @@
+const darkTheme ={
+    label: 'Dark',
+    background: 'black',
+    textColor: 'white',
+    typeBoxtext: 'grey',
+    errorColor: "red"
+ }
+
+ const fledging = {
+   label: "Fledging",
+   background: "#3B3635",
+   textColor: '#FC6E83',
+   typeBoxtext: '#8E5568',
+   errorColor: "red"
+ }
+
+ const copper = {
+   label: "Copper",
+   background: "#b46a55",
+   textColor: '#e7e0de',  //white
+   typeBoxtext: '#7ebab5', //skyblue
+   errorColor : "red"
+ }
+
+ const strawberry ={
+   label: "Strawberry",
+   background: "#f37f83",  //lighpink
+   textColor: "#fcfcf8",  //white
+   typeBoxtext: "#e53c58",  //darkpink
+   errorColor : "#fcd23f"  // yellow
+ }
+
+ const aether = {
+   label: "Aether",
+   background: "#101820",  
+   textColor: "#cf6bdd",  //white
+   typeBoxtext: "#eedaea",  //subcolor pink
+   errorColor : "#e3002b"
+ }
+
+ const Blue_Dolphine={
+label : "Blue Dolphone",
+background: "#003950",
+textColor: "#00e4ff",
+typeBoxtext: "#d1a5fd",
+errorColor: "#ff8188"
+ }
+
+ const Grey = {
+  label: "Grey",
+  background: "#dadbdc",
+  textColor: "#3846b1",
+  typeBoxtext: "#ae185e",
+  errorColor: "#306230"
+ }
+
+ const Future_Funk = {
+label: "Future Funk",
+background: "#2e1a47",
+textColor: "#c18fff",
+typeBoxtext: "#f7f2ea",
+errorColor: "#bd1c66"
+ }
+
+ const taro = {
+  label: "Taro",
+background: "#b3baff",
+textColor: "#130f1a",
+typeBoxtext: "#6f6c91",
+errorColor: "#ffe23e"
+ }
+
+ const menthol = {
+  label: "Menthol",
+  background: "#00c18c",
+  textColor: "#186544",
+  typeBoxtext: "#99fdd8",
+  errorColor: "#e03c3c"
+ }
+
+ export const themeOptions = [
+    {label: 'Dark', value: darkTheme},
+    {label:"Taro", value: taro},
+    {label:"Strawberry", value: strawberry},
+    {label:"Aether", value: aether},
+    {label:"Blue Dolphone", value: Blue_Dolphine},
+    {label:"Grey", value: Grey},
+    {label:"Future Funk", value: Future_Funk},
+    {label: 'Menthol', value: menthol},
+   {label: 'Copper', value: copper},
+    {label: 'Fledging', value: fledging}
+ ];
